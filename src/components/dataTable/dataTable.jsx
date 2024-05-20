568abb24725dba596525b3efa09a2c2db753e01b
Ashan Rajapaksha <ashanrajapaksha954@gmail.com>
2024-05-20 22:33:48 +0530

    feat: Createcustom hooks for fetch data

--- src/components/dataTable/dataTable.jsx
+++ src/components/dataTable/dataTable.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useFetch from "../../customHooks/useFetch";
 
 function DataTable() {
+  const url = "http://localhost:3000/bookInventry?page=1&limit=5";
+  const { data, loading, error } = useFetch(url);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
     <div className="table-wrap">
       <table class="table">
@@ -14,20 +21,15 @@ function DataTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            <td>@mdo</td>
-          </tr>
+          {data.books.map((book) => (
+            <tr>
+              <th scope="row">{book.title}</th>
+              <td>{book.author}</td>
+              <td>{book.ISBN}</td>
+              <td>$ {book.price}</td>
+              <td>@mdo</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
